442d1649074f3a3f17d2da05ec3f8da673152356
Kunal Khan <kunalkhanx@gmail.com>
2023-04-09 23:51:50 +0530

    Allowed cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const routes = require('./routes')
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
-
+app.use(cors())
 app.use('/api', routes)
 
 
